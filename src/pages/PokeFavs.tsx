f6aa7f4cda075917822f093b4dedcd1f111e38b3
Maria del Mar Lamilla <lamilla767@gmail.com>
2022-07-14 12:24:55 -0500

    add style

--- src/pages/PokeFavs.tsx
+++ src/pages/PokeFavs.tsx
@@ -8,7 +8,7 @@ const PokeFavs: React.FunctionComponent<IPokeFavsProps> = (props) => {
     <>
                 <header className="bg-white shadow">
                 <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <h1 className="text-3xl font-bold text-gray-900">Here your favs pokemons</h1>
+                    <h1 className="text-3xl font-bold text-blue-900">Here your favs pokemons</h1>
                 </div>
             </header>
             <main>
--- src/pages/PokeList.tsx
+++ src/pages/PokeList.tsx
@@ -8,9 +8,10 @@ interface IPokeListProps {
 const PokeList: React.FunctionComponent<IPokeListProps> = (props) => {
 
     const [pokemons, setPokemons] = useState([])
+    const [search, setSearch] = useState("")
 
     const loadData = () => {
-        axios.get('https://pokeapi.co/api/v2/pokemon?limit=3&offset=0')
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
             .then(response => {
                 for (let i = 0; i < response.data.results.length; i++) {
                     axios.get(response.data.results[i].url)
@@ -25,42 +26,45 @@ const PokeList: React.FunctionComponent<IPokeListProps> = (props) => {
 
     return (
         <>
+
             <header className="bg-white shadow">
                 <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <h1 className="text-3xl font-bold text-gray-900">Find Your Pokemon</h1>
+                    <h1 className="text-3xl font-bold text-blue-900">Find Your Pokemon</h1>
                 </div>
             </header>
             <main>
-                <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                <div className="max-w-7xl mx-auto py-2 sm:px-6 lg:px-3">
                     <div className="px-4 py-6 sm:px-0">
-                        <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
-
-                            {pokemons.map((pokemon, index) => {
-                                //return <p key={index}> {pokemon.name}</p>
-                                return <>
-                            <div className="flex bg-indigo-50 px-4">
-                                <div className="w-60 max-w-sm overflow-hidden rounded-xl bg-white shadow-md duration-100 hover:scale-105 hover:shadow-xl">
-                                    <img src={pokemon.sprites.front_default} alt="plant" className="h-auto w-full" />
-                                    <div className="p-5">
-                                        <p className="text-medium mb-5 text-gray-700">{pokemon.name}</p>
-                                        <div>
-                                        <button className="w-19 rounded-md bg-indigo-600  py-1 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">{pokemon.name}</button>
-                                        <button className="w-19 rounded-md bg-indigo-600  py-1 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">{pokemon.name}</button>
-                                        </div>
-                                    </div>
-                                </div>
+                        <div className="p-8 border-4 border-solid border-gray-200 rounded-lg h-full">
+                            <div className="flex justify-left space-x-4 relative z-0 w-full mb-6  ">
+                                <input type="text" id="searchPokemon" className="block py-2.5 px-0 w-60 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                <label for="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Find a pokemon</label>
+                                <button className="w-20 rounded-md bg-indigo-600  py-1 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">search</button>
                             </div>
-                            </>    
-                            })}
+                            <div className="grid grid-cols-4 gap-3">
+                                {pokemons.map((pokemon, index) => {
+                                    //return <p key={index}> {pokemon.name}</p>
+                                    return <>
 
+                                        <div className="border-4 border-dashed border-gray-200 rounded-lg h-full">
+                                            <img src={pokemon.sprites.front_default} alt="plant" className="h-auto w-full" />
+                                            <div className="p-5">
+                                                <p className="flex justify-center text-medium mb-5 text-gray-700 ">{pokemon.name}</p>
+                                                <div className="flex justify-center space-x-4">
+                                                    {pokemon.types.map(pokeType => <button className="w-20 rounded-md bg-indigo-600  py-2 text-indigo-100 hover:bg-indigo-500 hover:shadow-md duration-75">{pokeType.type.name}</button>)}
 
+                                                </div>
+                                            </div>
+                                        </div>
 
-
-
+                                    </>
+                                })}
+                            </div>
                         </div>
                     </div>
                 </div>
             </main>
+
         </>
     );
 };
